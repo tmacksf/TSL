445c0cbba9da93743b4255cf3a9c9408976c2518
tmacksf <33499837+tmacksf@users.noreply.github.com>
2023-08-26 15:00:55 +0100

    Sort
    Changed documentation. Included a max data size.

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 void swap(void *a, void *b, U32 size) {
-  char buffer[size];
+  char buffer[MAX_DATA_SIZE];
 
   memcpy(buffer, a, size);
   memcpy(a, b, size);
@@ -11,8 +11,12 @@ void swap(void *a, void *b, U32 size) {
 void quicksort(void *data, U32 count, U32 dataSize,
                int (*cmp)(const void *, const void *, void *), void *extra) {
 
-  // TODO: Some optimisation to make sure the pivot is less than the end of the
-  // list and greater than the start?
+  /* TODO: Some optimisation to make sure the pivot is less than the end of the
+   * list and greater than the start? */
+  if (dataSize > MAX_DATA_SIZE) {
+    printf("Data size too large\n");
+    return;
+  }
 
   if (count < 2)
     return;
@@ -20,7 +24,7 @@ void quicksort(void *data, U32 count, U32 dataSize,
   U32 pivot = count / 2;
   U32 i = 1;
   U32 j = count - 1;
-  // Don't need to do this but its easier to understand
+  /* Don't need to do this but its easier to understand */
   swap(data, data + pivot * dataSize, dataSize);
 
   pivot = 0;
@@ -40,7 +44,7 @@ void quicksort(void *data, U32 count, U32 dataSize,
   }
 
   swap(data, data + j * dataSize, dataSize);
-  // front partition
+  /* front partition */
   quicksort(data, j, dataSize, cmp, extra);
   quicksort(data + j * dataSize + dataSize, count - j, dataSize, cmp, extra);
 }
@@ -48,7 +52,7 @@ void quicksort(void *data, U32 count, U32 dataSize,
 void bubblesort(void *data, U32 count, U32 dataSize,
                 int (*cmp)(const void *, const void *, void *), void *extra) {
 
-  // pass over the array
+  /* pass over the array */
   for (int pass = 0; pass < count - 1; pass++) {
     for (int i = 0; i < count - pass; i++) {
       if (cmp(data + i * dataSize, data + (i + 1) * dataSize, extra) > 0) {
--- src/sort/sort.h
+++ src/sort/sort.h
@@ -4,17 +4,19 @@
 #include "../base/base.h"
 #include <string.h>
 
+#define MAX_DATA_SIZE 10000 /* Max data size */
+
 void swap(void *a, void *b, U32 size);
 
 void quicksort(void *data, U32 count, U32 dataSize,
                int (*cmp)(const void *, const void *, void *), void *extra);
 
-void rsort(); // radixsort
+void rsort(); /* radixsort */
 
 void bubblesort(void *data, U32 count, U32 dataSize,
                 int (*cmp)(const void *, const void *, void *), void *extra);
 
-void msort(); // mergesort
+void msort(); /* mergesort */
 
-void hsort(); // heapsort
+void hsort(); /* heapsort */
 #endif
